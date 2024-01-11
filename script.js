9b82e773b2ace8eca1b11934b9fe17bde3805512
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2024-01-11 17:11:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const showMoreBtn = document.getElementById("show-more-btn");
+var imageContainer = document.getElementById("imageContainer");
+imageContainer.innerHTML = "";
+
+let page = 1;
+
+async function searchImages() {
+  var searchTerm = document.getElementById("searchTerm").value;
+
+  var accessKey = "-bUcgjmc58_zN8vLvBdS0uF-kvaQGrjChVMJD0LxJ3s"; // Replace with your Search Engine ID
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${searchTerm}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (page == 1) {
+    imageContainer.innerHTML = "";
+  }
+  const results = data.results;
+
+  results.map((image) => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image.urls.small;
+    const imgLink = document.createElement("a");
+    imgLink.href = image.links.html;
+    imgLink.target = "_blank";
+
+    imgLink.appendChild(imgElement);
+    imageContainer.appendChild(imgElement);
+  });
+}
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
